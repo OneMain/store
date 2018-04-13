ecacb227e311ace913ecc8a890a059beff59f30d
cherry <cherry_822@126.com>
2018-04-13 16:51:09 +0800

    修该登录页面

--- Gemfile
+++ Gemfile
@@ -43,5 +43,7 @@ group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  # BDD测试框架
+  gem "rspec-rails"
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,24 @@ module ApplicationHelper
     # => <div id=cart>render xxxxx</div>
     content_tag("div",attributes,&block)
   end
+
+  # 显示闪现消息,显现消息出现再下次请求才会出现如果想本次请求出现flash[:now]
+  #flash方法的消息会从保存到下一个action，和redirct_to方法一起使用
+  #flash.now 方法的详细只会在当前视图显示，不会保存到下一个action，和render方法一起使用
+  def flash_message
+    debugger
+    flash_messages = []
+    flash.each do |type,message|
+      type = :success if type.to_sym == :notice
+      type = :danger  if type.to_sym == :alert
+      text = content_tag(:div,link_to(raw('<i class = "fa fa-close"></i>'),"#",
+                                      :class => 'close',"data-dismiss" => "alert") + message,
+                                      class: "alert alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+
+
+
 end
